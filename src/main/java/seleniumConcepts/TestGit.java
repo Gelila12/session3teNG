a3910f833a7165b23c4f30ad0bdc3c11ae9dc27b
Gelila12 <75943633+Gelila12@users.noreply.github.com>
2021-07-06 21:09:37 -0500

    5th commit

---
+++ src/main/java/seleniumConcepts/TestGit.java
@@ -0,0 +1,12 @@
+package seleniumConcepts;
+
+public class TestGit {
+	
+	public static void main (String[]arg) {
+		printMe();
+		}
+
+	public static void printMe() {
+		System.out.println("Test Git");
+	}
+}
